d1e7aae74d0e73507ccce0cda4be4ca9db199eee
Piero-Jeri <piero.jeri@tecsup.edu.pe>
2025-09-16 16:54:37 -0500

    Npcs y Players

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Player player1 = new(10);
+    public Player player2 = new(10);
     void Start()
     {
-        
+        Player player3 = player1 + player2;
+        Player player4 = player2 / 2;
+        Player player5 = player1 * player2;
+
+        print(player1 = player2);
+        print(player3 > player2);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    //https://github.com/sowTecsup/S5PAV_JN
 }
